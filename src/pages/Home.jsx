cffcbf29ea9c8df15341a670c37aebffa9b04545
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-04 18:00:49 +0000

    use react bootstrap npm to structure homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { Row, Col, Container, Button } from "react-bootstrap"
+import './Home.css'
+import Me from '../images/profile-picture.jpg' 
 
 const Home = () => {
     return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
         <Header />
-        <h1>What is up!!</h1>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate explicabo velit animi porro, sint voluptatibus voluptatum aliquam aperiam assumenda et sapiente repudiandae placeat reprehenderit quia nihil harum, quod recusandae consectetur.</h1>
+        <Container>
+            <Row className="mt-5">
+                <Col xs={12} md={6}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis aut odio repellat? Repellat consequatur eaque quisquam cum error labore quam excepturi molestias, aperiam quasi deleniti voluptatem nihil recusandae facere animi.</Col>
+                <Col xs={12} md={6}>
+                    <img src={Me} alt="Me, driving a boat on a lake" className="portfolio-image"/>
+                </Col>
+            </Row>
+        </Container>
+        <Footer />
     </div>
     )
 
