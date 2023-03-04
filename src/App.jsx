0cb3ae6926a8ca301d39cc4cb91b37fd6498dd22
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-04 15:50:55 +0000

    link each page via react router + add styling to index

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
-import Navbar from './components/Navigation'
+import Header from './components/Header'
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
 
 function App() {
-  
-
   return (
     <div>
-      <Navbar />
-      
+      <Header/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   )
 }
