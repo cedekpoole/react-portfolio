776fb917a7967cb537297b76989a6c414246ad10
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-06 14:38:55 +0000

    add Footer to App() function and add background stlying classes to css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import About from './pages/About'
@@ -18,6 +19,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
