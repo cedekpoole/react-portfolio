15dda869275925dd3f24e664bb67ac2ec2e83f06
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-07 14:58:11 +0000

    update project page and add background styling

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,25 +1,32 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import ProjectCard from "../components/ProjectCard";
-import { Row, Container } from "react-bootstrap";
-import './Projects.css'
+import { Row, Container, Col } from "react-bootstrap";
+import "./Projects.css";
+import "../components/ProjectCard.css";
 
 import Cards from "../project-data.json";
 
 const Projects = () => {
   return (
-    <div>
-      <div className="background-2"></div>
-      <div className="background2-reverse"></div>
-      <h1 className="text-dark">Projects</h1>
+    <div className="bg-color">
+      <div className="red-bg-path"></div>
       <Container>
         <Row>
-          {Cards.map(proj => (
+          <Col xs={12}>
+            <div className="section-header mb-4 position-relative mt-3">
+              <i className="fa-regular fa-folder-open"></i>
+              <br />
+              Previous <span className="color-change">Projects</span>.
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          {Cards.map((proj) => (
             <ProjectCard
               id={proj.id}
               key={proj.id}
               title={proj.title}
-              link={proj.link}
+              link={proj.link[0]}
+              github={proj.link[1]}
               image={proj.image}
               alt={proj.alt}
               text={proj.text}
