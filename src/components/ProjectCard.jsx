6dff059387fe593c1710d532e4949599aa9b1bdc
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-07 14:24:18 +0000

    add buttons to project cards and update card styling

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,28 +1,33 @@
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, Button } from "react-bootstrap";
 import "./ProjectCard.css";
-import React from 'react'
 
 function ProjectCard(props) {
   return (
-    <Col xs={6} md={4} lg={3} className="my-2">
-      <Card className="h-100 project-link">
-        <a href={props.link} target="_blank">
-          <div>
-            <Card.Img
-              variant="top"
-              src={props.image}
-              alt={props.alt}
-              className="img-fluid proj-img"
-            />
-            <Card.Body>
-              <Card.Title>{props.title}</Card.Title>
-              <Card.Text className="text-small">{props.text}</Card.Text>
-            </Card.Body>
-          </div>
-        </a>
+    <Col xs={12} md={6} lg={4} className="my-2">
+      <Card className="h-100 project-card">
+        <div>
+          <Card.Img
+            variant="top"
+            src={props.image}
+            alt={props.alt}
+            className="img-fluid proj-img"
+          />
+          <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Text className="text-small">{props.text}</Card.Text>
+            <a href={props.link} target="_blank">
+              <Button variant="success" className="button-style">Online</Button>
+            </a>
+            <a href={props.github} target="_blank">
+              <Button className="button-style button2">
+                <i className="fa-brands fa-github icon"></i>
+              </Button>
+            </a>
+          </Card.Body>
+        </div>
       </Card>
     </Col>
   );
-};
+}
 
 export default ProjectCard;
