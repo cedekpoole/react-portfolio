97f41de69c60a34c1f307eae4dffae7438f33aee
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-07 14:59:48 +0000

    create eyes via css and place portfolio image onto Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,23 +9,27 @@ const Home = () => {
     <div>
         <div className="background"></div>
         <div className="background-2"></div>
-        <Header />
-        <Container className="position-relative">
-            <Row className="mt-5">
-                <Col xs={12} md={6} className="mt-4">
-                    <h1>Welcome to my <span className="color-change">portfolio</span></h1>
-                
-                {/* <div className="typewriter">
-                    Sit back, and relax. An aspiring Full-Stack Dev
-                    </div> */}
-        
+        <Container fluid>
+            <Row className="mt-5 position-relative">
+                <Col xs={12} md={6} className="mt-4 ms-2">
+                    <div className="dark-col">
+                        <h1 className="mb-4 ms-2">Welcome to my <span className="color-change">portfolio</span>.</h1>
+                        <h4 className="mb-2 ms-4"> My name is <span className="color-change">CAMERON EDEK POOLE</span></h4>
+                        <p className="ms-4">An aspiring Full-Stack Developer :)</p>
+                    </div>
+                    <div className="circle">
+                    <div className="circle1"></div>
+                    </div>
+                    <div className="circle circle-right">
+                    <div className="circle1"></div>
+                    </div>
                 </Col>
-                <Col xs={12} md={6}>
-                    {/* <img src={Me} alt="Me, driving a boat on a lake" className="portfolio-image"/> */}
+                <Col xs={12} md={5} className="ms-1">
+                    <img src={Me} alt="Me, driving a boat on a lake" className="portfolio-image"/>
+                    <i className="fa-brands fa-git-alt floating"></i>
                 </Col>
             </Row>
         </Container>
-        <Footer />
     </div>
     )
 
