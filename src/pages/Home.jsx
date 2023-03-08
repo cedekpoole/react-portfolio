2c4c0476eacf8ae96b8395483a7bc1c1ac28cae0
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-08 14:27:05 +0000

    add margin to title and general refactoring of css for Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,25 +8,26 @@ const Home = () => {
     return (
     <div>
         <div className="background"></div>
-        <div className="background-2"></div>
         <Container fluid>
             <Row className="mt-5 position-relative">
                 <Col xs={12} md={6} className="mt-4 ms-2">
-                    <div className="dark-col">
-                        <h1 className="mb-4 ms-2">Welcome to my <span className="color-change">portfolio</span>.</h1>
-                        <h4 className="mb-2 ms-4"> My name is <span className="color-change">CAMERON EDEK POOLE</span></h4>
-                        <p className="ms-4">An aspiring Full-Stack Developer :)</p>
+                    <div>
+                        <h1 className="mb-4 ms-lg-2">Welcome to my <span className="color-change">portfolio</span>.</h1>
+                        <h4 className="mb-2 ms-lg-4"> My name is <span className="color-change">CAMERON EDEK POOLE</span>...</h4>
+                        <p className="ms-lg-5">An aspiring Full-Stack Developer :)</p>
                     </div>
-                    <div className="circle">
-                    <div className="circle1"></div>
-                    </div>
-                    <div className="circle circle-right">
-                    <div className="circle1"></div>
-                    </div>
+                    {/* <div className="ms-4 eyes">
+                        <div className="circle">
+                        <div className="circle1"></div>
+                        </div>
+                        <div className="circle circle-right">
+                        <div className="circle1"></div>
+                        </div>
+                    </div> */}
                 </Col>
                 <Col xs={12} md={5} className="ms-1">
-                    <img src={Me} alt="Me, driving a boat on a lake" className="portfolio-image"/>
-                    <i className="fa-brands fa-git-alt floating"></i>
+                    {/* <img src={Me} alt="Me, driving a boat on a lake" className="portfolio-image"/> */}
+                    {/* <i className="fa-brands fa-git-alt floating"></i> */}
                 </Col>
             </Row>
         </Container>
