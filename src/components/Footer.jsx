74173171dd1e07b52020268b347b6e63168a8495
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-10 16:11:39 +0000

    add links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,13 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <footer className="footer fixed-bottom">
-            <span className="right"><i className="fa-regular fa-copyright"></i> cedekpoole</span>
+        <footer className="footer fixed-bottom d-flex justify-content-between">
+            <div className="links d-flex ms-lg-4 mt-2">
+                <a href="https://github.com/cedekpoole" target="_blank"><i class="fa-brands fa-github mx-3 fa-fade"></i></a>
+                <a href="https://www.linkedin.com/in/cam-edek-poole/" target="_blank"><i class="fa-brands fa-linkedin mx-3 fa-fade"></i></a>
+                <a href="https://discordapp.com/users/1003033771618086922" target="_blank"><i class="fa-brands fa-discord mx-3 fa-fade"></i></a>
+            </div>
+           <p className="m-2 lead"><i className="fa-regular fa-copyright"></i> cedekpoole</p>
         </footer>
     )
 }
