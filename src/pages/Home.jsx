7b091710156f8cfa466df41e16a1111fa2b5df1a
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-11 15:47:16 +0000

    add media query for img background and change column sizes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,18 +7,18 @@ const Home = () => {
     <div>
         <Container fluid>
             <Row>
-                <Col lg className="mt-4">
-                    <h1 className="mb-4 ms-lg-2">Welcome to my <span className="color-change">portfolio</span>.</h1>
+                <Col xl className="mt-4">
+                    <h1 className="mb-4 ms-lg-2 display-1">Welcome to my <span className="color-change">portfolio</span>.</h1>
                     <Row>
-                        <Col md>
-                        <h5>My name is <span className="color-change">CAMERON EDEK POOLE</span>, an aspiring full-stack developer:)</h5>
+                        <Col xs={12} lg={8}>
+                        <h4 className="mb-3 lh-base">My name is <span className="color-change">CAMERON EDEK POOLE</span>, an aspiring full-stack developer:)</h4>
                         </Col>
-                        <Col md>
-                    <Button className="mb-4 cv">Download CV</Button>
+                        <Col xs={12} lg={4} className="text-xl-center">
+                    <Button className="mb-2">Download CV</Button>
                         </Col>
                     </Row>
                 </Col>
-                <Col lg className="position-relative">
+                <Col xl className="position-relative">
                     <div className="img-background"></div>
                     <img src={image} alt="Me, wearing a hat and a coat" className="portfolio-image"/>
                    
